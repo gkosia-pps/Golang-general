59986b9b3a519b55864234f7882e04b794f693a7
gkosia-pps <gavrilis.kosiari@pepperstone.com>
2023-10-03 17:32:04 +0300

    until variadic functions

--- 08_handle_errors.go
+++ 08_handle_errors.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Error is an interface in go with a method signuture Error() string
+// I can implement the interface to create custom errors
+type customUserError struct {
+	name string
+}
+
+func (ue customUserError) Error() string {
+	return fmt.Sprintf("Error for user %v", ue.name)
+}
 
 type User struct {
 	name string
 }
 
+func getUserWithError() error {
+	return customUserError{
+		name: "AAAAAA",
+	}
+}
 func getUser(name string) (User, error) {
 
 	if name != "" {
@@ -13,12 +30,15 @@ func getUser(name string) (User, error) {
 		return User{
 			name: "Gab",
 		}, nil
+
 	} else {
 		return User{}, fmt.Errorf("No username passed")
+		// OR use errorsw package
+		//return User{}, errors.New("This is an error from package")
 	}
 }
 
-func main() {
+func main_08() {
 
 	// handle error
 	u, err := getUser("")
@@ -28,5 +48,12 @@ func main() {
 	}
 
 	fmt.Println(u.name)
+
+	// use the custom error
+	// custom errors allows us to store extra information that we can use to format the error message
+	err = getUserWithError()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 }
---
+++ 09_arrays.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// arrays has fix size and store specific datatype
+
+// ...int: slicer
+func variadic_fun(nums ...int) {
+	fmt.Println(nums)
+
+}
+
+func main() {
+
+	my_array := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	for i := 0; i < len(my_array); i++ {
+		fmt.Println(my_array[i])
+	}
+
+	// slices have dynamic size
+	// slices are build on top of arrays
+	// sclicers are passed by reference in the functions
+
+	// create slicer ontop of array
+	scl := my_array[0:5]
+	fmt.Println(scl)
+
+	// create a new slicer
+	// make(datatype, initial size, capacity) capacity can be ignored
+	// if pass the lenght it will copy the data to other memory place and expand
+	my_scl := make([]int, 5, 10)
+	fmt.Println(len(my_scl), cap(my_scl))
+
+	// call variadic function
+	// https://www.youtube.com/watch?v=un6ZyFkqFKo
+	variadic_fun(1, 1, 2, 3)
+}
